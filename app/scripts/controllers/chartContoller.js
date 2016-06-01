3e2f1d9c758d54b2389ae98ee4c9244530138e7d
naveenjafer <f2013019@hyderabad.bits-pilani.ac.in>
2016-06-01 23:16:34 +0530

    added pie chart

--- app/scripts/controllers/chartContoller.js
+++ app/scripts/controllers/chartContoller.js
@@ -10,10 +10,34 @@ angular.module('enviroCarApp')
   .controller('ChartCtrl', ['$state','$scope','$http','$rootScope','$timeout','$stateParams','factorysingletrack', function ($state, $scope,$http,$rootScope, $timeout,$stateParams,factorysingletrack) {
     console.log("came to controller of charts");
     //console.log("IMPORTANT VALUE"+$stateParams.trackid)
+    $scope.options_pie = {
+           chart: {
+               type: 'pieChart',
+               height: 420,
+               x: function(d){return d.key;},
+               y: function(d){return d.y;},
+               showLabels: true,
+               duration: 500,
+               labelThreshold: 0.01,
+               labelSunbeamLayout: true,
+               legend: {
+                   margin: {
+                       top: 5,
+                       right: 35,
+                       bottom: 5,
+                       left: 0
+                   }
+               }
+           }
+       };
+
+       $scope.data_pie = [
+
+       ];
     $scope.options = {
       chart: {
            type: 'lineWithFocusChart',
-           height: 450,
+           height: 470,
            margin : {
                top: 20,
                right: 50,
@@ -60,6 +84,46 @@ angular.module('enviroCarApp')
         paths: {},
         markers: {}
       });
+    $scope.piechartselected = "Speed";
+
+    var piechartsdata = {
+      'Speed': [0,0,0,0,0,0],
+      'Calculated MAF': [0,0,0,0,0,0],
+      'Engine Load': [0,0,0,0,0,0],
+      'Consumption': [0,0,0,0,0,0],
+      'Intake Temperature': [0,0,0,0,0,0]
+    };
+
+    var rangeobjects = {
+    'Speed': [[0,20,40,60,80,100],],
+    'Calculated MAF': [[0,5,10,15,20,25],],
+    'Engine Load': [[0,100,200,300,400,500],],
+    'Consumption':[[0,4,8,12,16,20],],
+    'Intake Temperature': [[0,10,20,30,40,50],]
+    }
+
+
+    $scope.selecteditemchanged = function()
+    {
+      var temp_obj = {};
+      for(var i = 0; i<=5 ; i++)
+      {
+        temp_obj['y'] = piechartsdata[$scope.piechartselected][i];
+        var content;
+        if(i != 5)
+        {
+          content = rangeobjects[$scope.piechartselected][0][i] + "-" + rangeobjects[$scope.piechartselected][0][i+1]+" "+rangeobjects[$scope.piechartselected][1];
+        }
+        else
+        {
+          content = "> " + rangeobjects[$scope.piechartselected][0][i] + " " + rangeobjects[$scope.piechartselected][1];
+        }
+        temp_obj['key'] = content;
+        $scope.data_pie[i] = temp_obj;
+        temp_obj = {}
+      }
+    };
+    //the initialy selected value of piechart params.
     var url = "https://envirocar.org/api/stable/users/";
     url = url + $rootScope.globals.currentUser.username + "/tracks/";
     $http.defaults.headers.common = {'X-User': $rootScope.globals.currentUser.username, 'X-Token': $rootScope.globals.currentUser.authdata};
@@ -79,18 +143,33 @@ angular.module('enviroCarApp')
             var datafinal = [];
             var len_data = data.data.features.length;
             var phenoms = ["Speed","Calculated MAF","Engine Load","Consumption","Intake Temperature"];
+            $scope.piechartoptions = phenoms;
             var colors = ["#ff9933", "#ffff00", "#00cc00", "#440044", "#ff3300"]
             for( var j= 0 ; j< phenoms.length; j++)
             {
               var dat=[];
+
               for(var i=0;i<len_data;i++)
               {
-
+                if(i == 0)
+                      rangeobjects[phenoms[j]][1] = data.data.features[i].properties.phenomenons[phenoms[j]].unit;
                   var date = new Date(data.data.features[i].properties.time);
                   var date_as_ms = date.getTime();
                   if(data.data.features[i].properties.phenomenons[phenoms[j]])
                   {
                       var speed = data.data.features[i].properties.phenomenons[phenoms[j]].value;
+                     for(var k = 5; k>=0 ; k--)
+                      {
+
+                        if(speed >= rangeobjects[phenoms[j]][0][k])
+                        {
+                          console.log(rangeobjects[phenoms[j]][1]+"READABLE");
+                          piechartsdata[phenoms[j]][k]++;
+                          console.log(piechartsdata[phenoms[j]][k]+"value")
+                          break;
+                        }
+                      }
+
                   }
                   else
                   {
@@ -98,6 +177,26 @@ angular.module('enviroCarApp')
                   }
                   dat.push([date_as_ms,speed]);
                   var data_to_push = {"key": phenoms[j],"values":dat,"color": colors[j]};
+              }
+              if(phenoms[j] == "Speed")
+              {
+                var temp_obj = {};
+                for(var i = 0; i<=5 ; i++)
+                {
+                  temp_obj['y'] = piechartsdata[$scope.piechartselected][i];
+                  var content;
+                  if(i != 5)
+                  {
+                    content = rangeobjects[$scope.piechartselected][0][i] + "-" + rangeobjects[$scope.piechartselected][0][i+1]+" "+rangeobjects[$scope.piechartselected][1];
+                  }
+                  else
+                  {
+                    content = "> " + rangeobjects[$scope.piechartselected][0][i] + " " + rangeobjects[$scope.piechartselected][1];
+                  }
+                  temp_obj['key'] = content;
+                  $scope.data_pie[i] = temp_obj;
+                  temp_obj = {}
+                }
               }
               datafinal.push(data_to_push);
             }
