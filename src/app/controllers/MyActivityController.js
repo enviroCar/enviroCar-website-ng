6f0c6bf92254c8be3f9d9320e63bba3236d7cc33
njafer <f2013019@hyderabad.bits-pilani.ac.in>
2016-06-20 00:25:45 +0530

    friends section added

--- src/app/controllers/MyActivityController.js
+++ src/app/controllers/MyActivityController.js
@@ -9,6 +9,7 @@ angular.module('app')
 .controller('MyActivityController', ['$scope', '$http', '$rootScope', '$state',
   'myactivity',
   function($scope, $http, $rootScope, $state, myactivity) {
+    $scope.show_no_my_activity = false;
     console.log("fired my activity")
     $scope.events = [];
     var eventshelper = []
@@ -29,15 +30,18 @@ angular.module('app')
           myactivity.yourself;
         $http.get(url + "limit=" + $scope.page_size + "&page=" + $scope.page)
           .success(function(data, status, headers, config) {
+            if (data.activities.length == 0) {
+              $scope.show_no_my_activity = true;
+            }
             for (var i = 0; i < data.activities.length; i++) {
               var helper = {};
               helper['time'] = data.activities[i].time;
               helper['name'] = data.activities[i].user.name;
               //  helper['profileurl'] = "https://envirocar.org/api/stable/users/"+helper['name']+"/avatar";
-              console.log(helper['profileurl']);
+              //console.log(helper['profileurl']);
               helper['date'] = new Date(data.activities[i].time).toLocaleString();
-              console.log(i);
-              console.log(data.activities[i]);
+              //  console.log(i);
+              //console.log(data.activities[i]);
 
               if (data.activities[i].type == "FRIENDED_USER") {
                 helper['type'] = 0;
@@ -51,9 +55,9 @@ angular.module('app')
                 helper['trackidlink'] = data.activities[i].track.id
               }
               eventshelper.push(helper);
-              console.log(helper);
+              //  console.log(helper);
             }
-            console.log($scope.events);
+            //console.log($scope.events);
             $scope.events = eventshelper
           })
       }
